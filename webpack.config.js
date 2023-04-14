675e08f1f1a18a9f8b2227ec7d75e2463ad122a9
mido <101448145+midoch@users.noreply.github.com>
2023-04-14 11:29:43 +0200

    added files

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    open: true,
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    port: 3001,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
